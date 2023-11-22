f5aa377bc19456690295a1b6b9f264f18a79251d
hamreliusfilip <91670593+hamreliusfilip@users.noreply.github.com>
2023-11-22 10:10:27 +0100

    Fix - Coordinates

--- EyeCoordinates.m
+++ EyeCoordinates.m
@@ -5,7 +5,16 @@ function [mouthx, mouthy, leftEye, rightEye] = EyeCoordinates(Eyes, Mouth)
 
     mouthCentroid = stats_Mouth.Centroid(1, :);
     mouthx = mouthCentroid(1);
-    mouthy = mouthCentroid(2);
+    mouthy = round(mouthCentroid(2));
+    
+    %figure
+    %imshow(Eyes)
+    
+    Eyes(mouthy-50:end, :) = 0;
+    
+    % Display the processed "Eyes" image
+    %figure
+    %imshow(Eyes);
     
     Rmin = 5;
     Rmax = 50;
@@ -13,39 +22,10 @@ function [mouthx, mouthy, leftEye, rightEye] = EyeCoordinates(Eyes, Mouth)
     leftEye = [1,1];
     rightEye = [1,1];
     
-    xled = Eyes(:,1)
+    [centersDark, radiiDark] = imfindcircles(Eyes,[Rmin Rmax],'ObjectPolarity','dark', 'Sensitivity', 0.95);
     
-    [centersDark, radiiDark] = imfindcircles(Eyes,[Rmin Rmax],'ObjectPolarity','dark', 'Sensitivity', 0.90);
+    leftEye = centersDark(1,:) 
+    rightEye = centersDark(2,:)
     
-    centersDark
-    
-    leftEye = centersDark(1,:); 
-    rightEye = centersDark(2,:);
-    
-    %for i = 1:length(centersDark) - 1
-    
-     %   for j = 2:length(centersDark)
-            
-      %      if((abs((centersDark(i,1) - (centersDark(j,1)))) < 100) && (abs((centersDark(i,1) - (centersDark(j,1)))) > 40))
-         
-       %         if ((centersDark(i,2) < (mouthy-10)) && (centersDark(j,2) < (mouthy-10)))
-           
-        %            if (abs(centersDark(i,2) - centersDark(j,2)) < 15) % Y
-                
-                    
-         %               leftEye = centersDark(i,:); 
-                       % rightEye = centersDark(j,:); 
-                          
-                  
-                
-           %         end 
-                
-            %    end 
-        
-            %end
-        
-       % end
-    
-    %end
     
 end
