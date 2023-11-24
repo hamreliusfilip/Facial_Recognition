3ef9dc38133fb8a9c0790d624d14c5cbd0c2d5cd
MilaShahnavaz <milsh107@student.liu.se>
2023-11-24 14:38:05 +0100

    Update pca.m
    Lite bättre

--- pca.m
+++ pca.m
@@ -1,46 +1,33 @@
+function [IMG_pca] = pca(IMG)
 % Define the path to your image folder
-imageFolderPath = '/Users/milashahnavaz/Desktop/TNM034/DB1';
+imageFolderPath = 'DB1';
 
 % Get a list of image files in the folder
-imageFiles = dir(fullfile(imageFolderPath, '*.jpg')); % Change '*.jpg' based on your image format
+imageFiles = dir(fullfile(imageFolderPath, '*.jpg')); 
 
-numImages = numel(imageFiles); % Get the number of images
+% Get the number of images
+numImages = numel(imageFiles);
 
-% Set the target size
-targetSize = [650, 650];
+targetsize = [650,650]
 
-% Initialize matrix to store image data
-imageData = zeros(numImages, prod(targetSize)); % Initialize matrix
+% Initialize matrix to store image data without reshaping
+imageData = zeros(numImages, prod(targetsize)); 
 
 % Process each image in the folder
-for i = 1:numImages
-    % Read the image
-    img = imread(fullfile(imageFolderPath, imageFiles(i).name));
+    for i = 1:numImages
     
-    % Convert to grayscale 
-    grayImage = rgb2gray(img);
+        % Read the image
+        img = imread(fullfile(imageFolderPath, imageFiles(i).name));
+        
+        % Convert image to column vector and store
+        imgVector = double(img(:)); 
+        
+        meanFace = mean(imgVector);
 
-    % Calculate padding needed to maintain aspect ratio
-    [rows, cols, ~] = size(grayImage);
-    padRows = max(0, targetSize(1) - rows);
-    padCols = max(0, targetSize(2) - cols);
+        % Subtract mean face from each image vector
+        normalizedData = imgVector - meanFace; 
     
-    % Pad the image to match the target size
-    paddedImage = padarray(grayImage, [padRows, padCols], 0, 'post');
-    
-    % Resize the padded image to the target size
-    resizedImage = imresize(paddedImage, targetSize);
-    
-    % Flatten the resized image into a vector and store in imageData matrix
-    imgVector = reshape(resizedImage, 1, []);
-    imageData(i, :) = imgVector;
-end
-
-% Calculate the mean face
-meanFace = mean(imageData, 1); % Calculate mean along the rows (each pixel position)
-
-% Subtract the mean face from each image vector
-normalizedData = imageData - meanFace; % Subtract mean face from each image vector
+    end
 
 % Transponera data för PCA-funktionen
 dataTranspose = normalizedData';
@@ -51,32 +38,32 @@ covarianceMatrix = cov(dataTranspose);
 % Utför egenvärdesanalys på kovariansmatrisen
 [eigenVectors, eigenValues] = eig(covarianceMatrix);
 
-% Sortera egenvärdena i fallande ordning
-[eigenValuesSorted, sortIdx] = sort(diag(eigenValues), 'descend');
-eigenVectorsSorted = eigenVectors(:, sortIdx);
-
-% Välj antalet främsta egenvärden (till exempel, för att använda de 50 främsta egenvärdena)
-numEigenFaces = 50;
-principalComponents = eigenVectorsSorted(:, 1:numEigenFaces);
-
-% Beräkna Eigenfaces genom att multiplicera principalComponents med normaliserad data
-eigenFaces = principalComponents' * normalizedData';
-
-% Beräkna den totala summan av egenvärden
-totalEigenValuesSum = sum(eigenValuesSorted);
-
-% Beräkna förklarad varians för varje egenvärde
-explainedVariance = cumsum(eigenValuesSorted) / totalEigenValuesSum;
-
-% Hitta det minsta antalet egenvärden som förklarar en viss procent av variansen
-desiredVarianceExplained = 0.95; % till exempel, 95% av variansen
-numImportantEigenValues = find(explainedVariance >= desiredVarianceExplained, 1);
-
-% Välj de viktigaste egenvärdena och deras motsvarande egenvärdesvektorer
-importantEigenVectors = eigenVectorsSorted(:, 1:numImportantEigenValues);
-
-% Använd de viktigaste egenvärdena för att transformera dina data
-importantFeatures = importantEigenVectors' * normalizedData';
+% % Sortera egenvärdena i fallande ordning
+% [eigenValuesSorted, sortIdx] = sort(diag(eigenValues), 'descend');
+% eigenVectorsSorted = eigenVectors(:, sortIdx);
+% 
+% % Välj antalet främsta egenvärden (till exempel, för att använda de 50 främsta egenvärdena)
+% numEigenFaces = 10;
+% principalComponents = eigenVectorsSorted(:, 1:numEigenFaces);
+% 
+% % Beräkna Eigenfaces genom att multiplicera principalComponents med normaliserad data
+% eigenFaces = principalComponents' * normalizedData';
+% 
+% % Beräkna den totala summan av egenvärden
+% totalEigenValuesSum = sum(eigenValuesSorted);
+% 
+% % Beräkna förklarad varians för varje egenvärde
+% explainedVariance = cumsum(eigenValuesSorted) / totalEigenValuesSum;
+% 
+% % Hitta det minsta antalet egenvärden som förklarar en viss procent av variansen
+% desiredVarianceExplained = 0.95; % till exempel, 95% av variansen
+% numImportantEigenValues = find(explainedVariance >= desiredVarianceExplained, 1);
+% 
+% % Välj de viktigaste egenvärdena och deras motsvarande egenvärdesvektorer
+% importantEigenVectors = eigenVectorsSorted(:, 1:numImportantEigenValues);
+% 
+% % Använd de viktigaste egenvärdena för att transformera dina data
+% importantFeatures = importantEigenVectors' * normalizedData';
 
 
 
