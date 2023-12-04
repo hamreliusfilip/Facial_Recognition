98430db0349fa122f416047123d97727a6170655
hamreliusfilip <91670593+hamreliusfilip@users.noreply.github.com>
2023-12-04 10:34:07 +0100

    New eye coordinates
    %Vanligt
    % Alla fungerar
    
    %ROTATE 5
    % Alla fungerar
    
    %ROTATE -5
    % Alla fungerar
    
    %TRANSLATE [30,0]
    % Alla fungerar
    
    %TRANSLATE [0,30]
    % 3 -> 2
    % 4 -> 13
    % 6 -> 1
    % 8 -> 10
    % 11 -> 1
    % 13 -> 1
    % 14 -> 2
    
    %SCALING +10%
    % 7 -> 4 (fel ögon)
    % 12 -> error
    
    %SCALING -10%
    % 4 -> error
    
    %Tone 30%
    % Alla fel utom 10an
    
    %Tone -30%
    % Alla fel

--- EyeCoordinates.m
+++ EyeCoordinates.m
@@ -4,7 +4,6 @@ function [leftEye, rightEye] = EyeCoordinates(Eyes, Mouth)
     stats_Mouth = regionprops("table", Mouth, "Centroid", "MajorAxisLength", "MinorAxisLength");
 
     mouthCentroid = stats_Mouth.Centroid(1, :);
-    mouthx = mouthCentroid(1);
     mouthy = round(mouthCentroid(2));
     
     Eyes(mouthy-50:end, :) = 0;
@@ -17,45 +16,49 @@ function [leftEye, rightEye] = EyeCoordinates(Eyes, Mouth)
     leftEye = centersDark(1,:);
     rightEye = centersDark(2,:);
     
-    
-    
-    % Kontrollera med vinkeln om vi ens behver ändra något öga. 
-    
     x1 = rightEye(1,1);
     x2 = rightEye(1,2);
     y1 = leftEye(1,1);
     y2 = leftEye(1,2);
-  
-    slope = (rad2deg((y2 - y1) / (x2 - x1)))
+   
+    slope = (rad2deg((y2 - y1) / (x2 - x1)));
     
-    if ((slope > 5) && (slope < -5) || (abs((rightEye(1,1) - leftEye(1,1))) < 100))
+    if ((slope > 5) && (slope < -5) || (abs((rightEye(1,1) - leftEye(1,1))) < 80) || (abs((rightEye(1,1) - leftEye(1,1))) > 150) )
 
-            for i = 2:length(centersDark) - 1
-
-                 if((abs((centersDark(i,1) - leftEye(1,1))) < 150) && ... 
-                     (abs((centersDark(i,1) - leftEye(1,1))) > 120)  && ...
+        
+        for i = 1:length(centersDark) 
+            if((leftEye(1,1) > 180))
+                    leftEye = centersDark(i,:);
+                     y1 = leftEye(1,1); 
+                     y2 = leftEye(1,2);  
+            end 
+        end 
+        
+            for i = 1:length(centersDark)
+                
+                if((abs((centersDark(i,1) - leftEye(1,1))) < 150) && ... 
+                     (abs((centersDark(i,1) - leftEye(1,1))) > 80)  && ...
                      (i ~= length(centersDark))) 
 
                         rightEye = centersDark(i,:);
 
                         x1 = rightEye(1,1); 
                         x2 = rightEye(1,2); 
-                        slope = abs(rad2deg((y2 - y1) / (x2 - x1)))
+                        slope = abs(rad2deg((y2 - y1) / (x2 - x1)));
                         
-                        if ((slope > 5) && (slope < -5) || (abs((rightEye(1,1) - leftEye(1,1))) < 100))
-                            
+                        if ((slope < 10) && (slope > -10) && (abs((rightEye(1,1) - leftEye(1,1))) < 130))
+                           
                             break;
                             
                         end
-                        
-
                  end
             end
     end 
     
-    if (centersDark(1,1) > centersDark(2,1)) %Om vänster ögat är det högra
-        leftEye = centersDark(2,:);
-        rightEye = centersDark(1,:);
+    if (rightEye(1,1) < leftEye(1,1))
+        temp = leftEye; 
+        leftEye = rightEye;
+        rightEye = temp;
     end
     
        warning('on');
--- Test_Main.m
+++ Test_Main.m
@@ -1,60 +1,60 @@
 
 % % clear 
      
-IMG_load = imread("DB1/db1_04.jpg");
+IMG_load = imread("DB1/db1_02.jpg");
 
 %IMG_Initial = imrotate(IMG_load, 5,'bilinear','crop');
-IMG_Initial = imrotate(IMG_load, -5,'bilinear','crop');
+%IMG_Initial = imrotate(IMG_load, -5,'bilinear','crop');
 %IMG_Initial = imtranslate(IMG_load, [30,0]);
 %IMG_Initial = imtranslate(IMG_load, [0,30]);
 %IMG_Initial = imresize(IMG_load, 1.1);
 %IMG_Initial = imresize(IMG_load, 0.9);
-%IMG_Initial = IMG_load.*1.3;
+IMG_Initial = IMG_load.*1.3;
 %IMG_Initial = IMG_load.*0.7;
  
 [IMG,leftEye,rightEye] = Face_Detection(IMG_Initial);
  
 img = Face_Alignment_Normalization(IMG,leftEye,rightEye); 
 
+subplot(1,2,1)
 imshow(IMG) 
 hold on
-scatter(leftEye(:,1),leftEye(:,2), 'r', 'filled');  % Reverse indices due to row-column ordering
-scatter(rightEye(:,1), rightEye(:,2), 'b', 'filled');
+scatter(leftEye(:,1),leftEye(:,2), 'SizeData', 120, 'Marker', 'o', 'MarkerEdgeColor', 'b', 'MarkerFaceColor', 'b'); 
+scatter(rightEye(:,1), rightEye(:,2), 'SizeData', 120, 'Marker', 'o', 'MarkerEdgeColor', 'r', 'MarkerFaceColor', 'r');
 
-figure
-hold off
-imshow(img)
-
+subplot(1,2,2) 
+imshow(img) 
     
 test = TestEigen(img);
 disp(test)
 
+%Vanligt 
+% Alla fungerar 
+
 %ROTATE 5
-% 6 -> 4 ögonen korrekta
-% 13 -> 1 ögonen korrekta
+% Alla fungerar
 
 %ROTATE -5
-% 4 -> höger öga
+% Alla fungerar 
 
 %TRANSLATE [30,0]
-% 13 -> 1 dock konstigt eftersom ögonen blir korrekta
+% Alla fungerar
 
 %TRANSLATE [0,30]
-% 3 -> 2 ögonen blir dock korrekta
-% 4 -> 6
-% 6 -> 5
+% 3 -> 2
+% 4 -> 13
+% 6 -> 1
 % 8 -> 10
+% 11 -> 1
+% 13 -> 1
+% 14 -> 2
 
 %SCALING +10%
-% 6 -> 4 ögonen korrekta
-% 7 -> 4 höger fel
-% 13 -> 7
+% 7 -> 4 (fel ögon) 
+% 12 -> error 
 
 %SCALING -10%
-% 2 -> 1 höger blir helt nära vänster
-% 4 -> 6 höger helt off
-% 6 -> 4 ögonen korrekt thooo
-% 10 -> 13 höger öga helt off
+% 4 -> error 
 
 %Tone 30%
 % Alla fel utom 10an
